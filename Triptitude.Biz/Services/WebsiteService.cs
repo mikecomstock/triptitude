338418cb2ee552221327690635f2a282093516df
Mike Comstock <mikecomstock@gmail.com>
2011-02-06 18:56:24 +0000

    thumb folders

--- Triptitude.Biz/Services/WebsiteService.cs
+++ Triptitude.Biz/Services/WebsiteService.cs
@@ -29,13 +29,15 @@ namespace Triptitude.Biz.Services
 
             string originalOutputPath = Path.Combine(websiteThumbnailRoot, "Original", websiteId + ".jpg");
             string smallOutputPath = Path.Combine(websiteThumbnailRoot, "Small", websiteId + ".jpg");
+            string mediumOutputPath = Path.Combine(websiteThumbnailRoot, "Medium", websiteId + ".jpg");
+            string largeOutputPath = Path.Combine(websiteThumbnailRoot, "Large", websiteId + ".jpg");
 
             string cutyCaptPath = ConfigurationManager.AppSettings["CutyCaptPath"];
 
             string arguments = string.Format(@"--url=""{0}"" --out=""{1}""", url, originalOutputPath);
             var proc = new Process()
             {
-                StartInfo = new ProcessStartInfo(cutyCaptPath, arguments) { UseShellExecute = false }
+                StartInfo = new ProcessStartInfo(cutyCaptPath, arguments) { UseShellExecute = false, CreateNoWindow = true }
             };
             proc.Start();
             proc.WaitForExit();
@@ -48,8 +50,14 @@ namespace Triptitude.Biz.Services
                     int cropHeight = (int)Math.Round((decimal)600 / 800 * original.Width, 0);
                     using (Image croppedOriginal = new Bitmap(original).Clone(new Rectangle(0, 0, cropWidth, cropHeight), original.PixelFormat))
                     {
-                        Bitmap small = ResizeImage(croppedOriginal, 200, 150);
+                        Bitmap small = ResizeImage(croppedOriginal, 100, 750);
                         small.Save(smallOutputPath, ImageFormat.Jpeg);
+
+                        Bitmap medium = ResizeImage(croppedOriginal, 200, 150);
+                        medium.Save(mediumOutputPath, ImageFormat.Jpeg);
+                        
+                        Bitmap large = ResizeImage(croppedOriginal, 300, 225);
+                        large.Save(largeOutputPath, ImageFormat.Jpeg);
                     }
                 }
 
