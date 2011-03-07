5a71c5080265d781314ffe894437cf8633930d57
Mike Comstock <mikecomstock@gmail.com>
2011-03-07 06:28:02 +0000

    scripts go in a scripts file

---
+++ Triptitude.Web/Scripts/triptitude.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    
+    $('input').placeholder();
+    
+    $('#search form').submit(function () {
+        doSearch();
+        return false;
+    });
+    $('#search form input').keyup(function () {
+        doSearch();
+    });
+
+    var timerid;
+    function doSearch() {
+        var form = this;
+        clearTimeout(timerid);
+        timerid = setTimeout(function () {
+            $('#search-results').load('/search', $('#search form').serialize());
+        }, 500);
+    }
+
+});
