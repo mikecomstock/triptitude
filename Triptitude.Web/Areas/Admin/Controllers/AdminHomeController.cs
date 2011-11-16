0f82cc7777462ef454f6b1762d82976b46a8b710
Mike Comstock <mikecomstock@gmail.com>
2011-11-15 23:44:28 -0500

    cleanup tags already in the database (fix from changing to parse by comma)

--- Triptitude.Web/Areas/Admin/Controllers/AdminHomeController.cs
+++ Triptitude.Web/Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using Triptitude.Biz.Models;
 using Triptitude.Biz.Repos;
 using Triptitude.Web.Helpers;
 
@@ -17,7 +18,6 @@ namespace Triptitude.Web.Areas.Admin.Controllers
         {
             var activitiesRepo = new ActivitiesRepo();
             var activities = activitiesRepo.FindAll();
-
             foreach (var activity in activities)
             {
                 if (activity.Tags != null) activity.Tags.Clear();
@@ -28,6 +28,31 @@ namespace Triptitude.Web.Areas.Admin.Controllers
             }
 
             activitiesRepo.Save();
+
+            var packingListItemsRepo = new PackingListItemsRepo();
+            var itemTagRepo = new ItemTagRepo();
+            var packingListItems = packingListItemsRepo.FindAll();
+            foreach (var packingListItem in packingListItems)
+            {
+                Tag tag = null;
+                if (!string.IsNullOrWhiteSpace(packingListItem.TagString))
+                {
+                    tag = new TagsRepo().FindOrInitializeAll(packingListItem.TagString).First();
+                }
+
+
+                var itemTag = itemTagRepo.FindOrInitialize(packingListItem.ItemTag.Item, tag);
+                packingListItem.ItemTag = itemTag;
+            }
+
+            packingListItemsRepo.Save();
+
+            var itemTagsToDelete = itemTagRepo.FindAll().Where(it => it.PackingListItems.Count() == 0);
+            foreach (var itemTag in itemTagsToDelete)
+            {
+                itemTagRepo.Delete(itemTag);
+            }
+            itemTagRepo.Save();
 
             return Redirect(Url.Admin());
         }
