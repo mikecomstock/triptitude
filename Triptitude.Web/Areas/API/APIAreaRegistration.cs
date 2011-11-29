e393a2da02f7e223d79ed8090b78aefe9118dc66
Mike Comstock <mikecomstock@gmail.com>
2011-11-29 00:41:40 -0500

    api for tags

---
+++ Triptitude.Web/Areas/API/APIAreaRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Triptitude.Web.Areas.API
+{
+    public class APIAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "API";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute("API_default", "API/V1/{controller}/{action}/{id}", new { action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ Triptitude.Web/Areas/API/Controllers/TagsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Triptitude.Biz.Repos;
+
+namespace Triptitude.Web.Areas.API.Controllers
+{
+    public class TagsController : Controller
+    {
+        //
+        // GET: /API/Tags/
+
+        public ActionResult Index()
+        {
+            var tagsRepo = new TagsRepo();
+            var tags = tagsRepo.FindAll().Where(t => t.ItemTags.Where(it => it.ShowInSearch).Count() > 0 && t.ShowInSearch).ToList().OrderBy(t => t.NiceName);
+
+            var result = from t in tags
+                         select new
+                                    {
+                                        t.Id,
+                                        t.Name,
+                                        t.NiceName,
+                                        Items = from it in t.ItemTags.Where(it => it.ShowInSearch).OrderBy(it => it.Item.Name)
+                                                select new
+                                                           {
+                                                               it.Item.Id,
+                                                               it.Item.Name
+                                                           }
+                                    };
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
