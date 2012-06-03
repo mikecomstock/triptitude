88a7470557a79b08f03dd0f4c7b2972e8af92717
Mike Comstock <mikecomstock@gmail.com>
2012-06-03 02:00:50 -0400

    use css to hide the trip bar on some pages (instead of viewbag)

--- Triptitude.Web/Controllers/HomeController.cs
+++ Triptitude.Web/Controllers/HomeController.cs
@@ -8,18 +8,15 @@ namespace Triptitude.Web.Controllers
     {
         public ActionResult Index()
         {
-            TripsRepo tripsRepo = new TripsRepo();
-            var trips = tripsRepo.FindAll().Where(t => t.ShowInSearch).OrderByDescending(t => t.Created_On).Take(10).ToList();
-            ViewBag.HideTripBar = true;
-            ViewBag.Trips = trips;
-
+            //TripsRepo tripsRepo = new TripsRepo();
+            //var trips = tripsRepo.FindAll().Where(t => t.ShowInSearch).OrderByDescending(t => t.Created_On).Take(10).ToList();
+            //ViewBag.Trips = trips;
             return View();
         }
 
-        public ActionResult Header(bool? hideTripBar)
+        public ActionResult Header()
         {
             ViewBag.CurrentUser = CurrentUser;
-            ViewBag.HideTripBar = hideTripBar;
             return PartialView();
         }
 
